80c1931a1c5ef9e0d1776b71bf1c23df1fd00ed0
Sandesh <itsmesandesh12@gmail.com>
2026-06-10 15:42:03 +0545

    Added parking system project files

---
+++ admin login.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int login() {
+    char user[20], pass[20];
+
+    printf("ADMIN LOGIN\n");
+    printf("Username: ");
+    scanf("%s", user);
+
+    printf("Password: ");
+    scanf("%s", pass);
+
+    if (strcmp(user, "admin") == 0 && strcmp(pass, "1234") == 0) {
+        printf("Login successful!\n");
+        return 1;
+    }
+
+    printf("Invalid credentials!\n");
+    return 0;
+}
